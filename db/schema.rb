bb55ecd20b37d13f5f8c49091e49d7aed0437c91
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-10-14 14:45:11 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_102142) do
+ActiveRecord::Schema.define(version: 2020_10_14_025412) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
