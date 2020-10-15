96681a1b4b22337ca156ddb69ac8a9545184efbd
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-10-15 21:55:13 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  validates :content, presence: true
   has_one_attached :image
   validates :content, presence: true, unless: :was_attached?
   def was_attached?
